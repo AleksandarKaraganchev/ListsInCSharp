b7535d37e4bc44c9e406c170f2b2004f049d8719
STUDENTS-1212-12 <fifkobg@gmail.com>
2023-03-01 11:17:48 +0200

    Add project files.

---
+++ ListFibonachi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListFibonachi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            List<int> listFib = new List<int>();
+            listFib.Add(1);
+            listFib.Add(1);
+            Console.Write("Въведи до кое число на фибоначи искаш да се изчисли:  " );
+            byte numFib = byte.Parse(Console.ReadLine());
+            for (int i = 2; i < numFib; i++)
+            {
+                listFib.Add(listFib[i - 2] + listFib[i - 1]);
+            }          
+            Console.WriteLine(string.Join(", ",listFib)); ;
+        }
+    }
+}
---
+++ Zadacha1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Колко числа ще въвеждаш?: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------------");
+            List<int> numbers = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write("Въведи число: ");
+                int item = int.Parse(Console.ReadLine());
+                numbers.Add(item);
+            }
+            //obrabotka
+            //dobavqne v sredata
+            //int newindex = n / 2;//sredata na chislata na lista
+            //numbers.Insert(newindex, 6);
+            //dobavqne v kraq na nai-maluk element
+            //int minElement = numbers.Min();
+            //numbers.Add(minElement);
+            //da iztriq 0 vuv vuvedeniq spisuk
+            while (numbers.Contains(0))
+            {
+                numbers.Remove(0);
+            }
+            Console.WriteLine("----------------------------------------");
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine($"numbers[{i}] = {numbers[i]}");
+            }
+            Console.WriteLine("----------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Броят на елементите е: {numbers.Count}");
+            Console.WriteLine("----------------------------------------");
+        }
+    }
+}
